27300eaf64b4c0ef10f16088394a4b85aeffd926
migu3 <miguel.garcia.santops16@gmail.com>
2025-08-01 23:35:21 -0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double hoursWorked = 10;
-        double totalHours = 100;
-        int daysToComplete = 10;
-        double minutesWorked = 0;
-        double hoursForDays = 0;
+
         Scanner sc = new Scanner(System.in);
         TimeConverter tc = new TimeConverter();
 
@@ -14,7 +10,7 @@ public class Main {
         System.out.println("""
                 Select the operation:
                 1 - How many hours per day I jave to work for a project
-                
+                2 - How many days your project will least
                 """);
         int operation = sc.nextInt();
 
@@ -22,13 +18,25 @@ public class Main {
         switch (operation){
             case 1:
                 System.out.println("How many hours the full project will least?");
-                totalHours = sc.nextInt();
+                double totalHours = sc.nextInt();
 
                 System.out.println("How many days you have to finish the project?");
-                daysToComplete = sc.nextInt();
+                int daysToComplete = sc.nextInt();
 
-                hoursForDays = tc.getHoursForDays(totalHours, daysToComplete);
+                double hoursForDays = tc.getHoursForDays(totalHours, daysToComplete);
                 System.out.println("You have to work "+hoursForDays+" hours per day to complete in "+daysToComplete+" days");
+
+            case 2:
+                System.out.println("How many hours per day you will work on this project?");
+                double hoursPerDay = sc.nextInt();
+
+                System.out.println("How many hours dos de project will least?");
+                totalHours = sc.nextInt();
+
+                int totalDays = tc.getDays(hoursPerDay,totalHours);
+
+                System.out.println("Working "+hoursPerDay+" hours per day you will finish the project in "+totalDays+"days");
+
         }
 
     }
--- src/TimeConverter.java
+++ src/TimeConverter.java
@@ -1,22 +1,22 @@
+
+
 public class TimeConverter {
-    private double hour = 0;
-    private double minutes = 0;
-    private int day;
-    private int months;
 
      double getMinutesInHour(double hour, double minutes) {
          return hour * 60;
 
     }
 
-     int getDays(double hour) {
-        int dayConverted = (int) (hour / 24);
-        return dayConverted;
+     double getHoursForDays(double totalHours, int daysToComplete){
+         return (totalHours/ daysToComplete);
     }
-     double getHoursForDays(double hour, int daysToComplete){
 
-         return (int) (hour/ daysToComplete);
+    int getDays( double hoursPerDay,double totalHours){
+
+        return (int) (totalHours/ hoursPerDay);
     }
+
+
 }
 
 
